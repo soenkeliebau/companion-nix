f603b146e6c8a6905ab2b74c9da9a545029a2018
Sönke Liebau <soenke.liebau@stackable.tech>
2023-09-27 09:03:09 +0200

    Additional patch and more yarn installs for submodules.

--- default.nix
+++ default.nix
@@ -9,6 +9,7 @@
 , matrix-sdk-crypto-nodejs
 , nixosTests
 , nix-update-script
+, runCommandLocal
 }:
 
 let
@@ -23,7 +24,7 @@ let
             rev = version;
             hash = "sha256-x1C0sVpvqUhKLTa0bxuueK6PbVdxYysSpVimcPPGsro=";
         };
-        patches = [ ./remove_vendored_respawn.patch ./fixed_version_string.patch ];
+        patches = [ ./remove_vendored_respawn.patch ./fixed_version_string.patch ./vendor_shared_libs.patch ];
     };
 
     yarnOfflineCache = fetchYarnDeps {
@@ -31,9 +32,14 @@ let
         hash = "sha256-KWUlyiz2+mu6ve567KUfsnHis2m4FgpqjX80lt+ress=";
     };
 
+    uiYarnOfflineCache = fetchYarnDeps {
+        yarnLock = "${src}/webui/yarn.lock";
+        hash = "sha256-fIOt8710tQhrJr1srUa0QSNDyWwegmqYE21YL/VWRSw=";
+    };
+
 in
 stdenv.mkDerivation rec {
-    inherit pname version src yarnOfflineCache;
+    inherit pname version src yarnOfflineCache uiYarnOfflineCache;
 
     nativeBuildInputs = [
         prefetch-yarn-deps
@@ -46,17 +52,36 @@ stdenv.mkDerivation rec {
         runHook preConfigure
 
         export HOME=$(mktemp -d)
-        export VERSION_STRING=""
+        export VERSION_STRING="${version_string}"
+
+        #Install deps for main project
         yarn config --offline set yarn-offline-mirror "$yarnOfflineCache"
         fixup-yarn-lock yarn.lock
         yarn install --frozen-lockfile --offline --no-progress --non-interactive --ignore-scripts
+
+        # Copy vendored lib
+        cp --recursive ${src}/vendor/respawn node_modules/
+
         patchShebangs node_modules/ bin/
 
+        # Install deps for webui project
+        yarn --cwd webui config --offline set yarn-offline-mirror "$uiYarnOfflineCache"
+        fixup-yarn-lock webui/yarn.lock
+        yarn --cwd webui  install --frozen-lockfile --offline --no-progress --non-interactive --ignore-scripts
+
+        # Copy vendored lib
+        mkdir webui/node_modules/@companion
+        cp --recursive ${src}/lib/Shared webui/node_modules/@companion/shared
+
+        patchShebangs webui/node_modules/ webui/bin/
         runHook postConfigure
     '';
 
     buildPhase = ''
         runHook preBuild
+
+        export VERSION_STRING="${version_string}"
+        export ELECTRON=0
 
         yarn --offline build:writefile
         yarn --offline dist
